792212a413f49b99d2c3dce2ad47d85356ca3675
Abanilla <manuel.abanilla@ciit.edu.ph>
2023-02-18 14:55:55 +0800

    1st Meeting

---
+++ Assets/Scripts/OtherObjectModifier.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OtherObjectModifier : MonoBehaviour
+{
+    public GameObject otherObject;
+    public float thing = 15f;
+
+    [SerializeField]
+    private float s = 3f;
+
+    protected float b = 6f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
